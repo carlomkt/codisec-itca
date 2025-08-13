827f21069adee033302333148b0d89378f228ee3
carlomkt <carlo.mkt@gmail.com>
2025-08-12 19:11:10 -0500

    Update ResponsablesPage.tsx

--- src/features/responsables/ResponsablesPage.tsx
+++ src/features/responsables/ResponsablesPage.tsx
@@ -1,3 +1,2 @@
 import React from 'react';
-const ResponsablesPage: React.FC = () => <div className="p-4">Responsables</div>;
-export default ResponsablesPage;
+export default function ResponsablesPage() { return <div className="p-4">Responsables</div>; }
