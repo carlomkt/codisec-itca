a6575411d2e9eca440bec4ab4ac6e9c8905197ed
carlomkt <carlo.mkt@gmail.com>
2025-08-12 19:08:26 -0500

    Update ActividadesPage.tsx

--- src/features/actividades/ActividadesPage.tsx
+++ src/features/actividades/ActividadesPage.tsx
@@ -1,3 +1,2 @@
 import React from 'react';
-const ActividadesPage: React.FC = () => <div className="p-4">Actividades ITCA (conectar a API)</div>;
-export default ActividadesPage;
+export default function ActividadesPage() { return <div className="p-4">Actividades ITCA (conectar a API)</div>; }
